6440e4f6270f75fc134082b65d82ed965a1da1d9
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-07-15 15:48:13 +0200

    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -1,52 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.List;
 
 public class Display extends JPanel {
     private final Movable camera;
-    private final BufferedImage buffer;
-    private final int[] pixels;
-    private final double[] xValues;
 
-    public Display(Movable camera) {
-        this.camera = camera;
-        buffer = new BufferedImage(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
-        pixels = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
-        xValues = new double[Config.SCREEN_WIDTH];
-        for(int i = 0; i < Config.SCREEN_WIDTH; i++) {
-            xValues[i] = ((2.0 * i / Config.SCREEN_WIDTH) - 1.0) * Config.ASPECT_RATIO;
-        }
+    public Display() {
+        camera = Scene.getCamera();
+        setDoubleBuffered(Config.DOUBLE_BUFFERING);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         long startTime = System.nanoTime();
-        Arrays.fill(pixels, 0, Config.SCREEN_SIZE / 2, Color.GRAY.getRGB());
-        Arrays.fill(pixels, Config.SCREEN_SIZE / 2, Config.SCREEN_SIZE - 1, Color.DARK_GRAY.getRGB());
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 0, Config.SCREEN_WIDTH, Config.HALF_HEIGHT);
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0, Config.HALF_HEIGHT, Config.SCREEN_WIDTH, Config.HALF_HEIGHT);
         Vector position = camera.getPosition(), forward = camera.getForward();
-        IntStream.range(0, Config.SCREEN_WIDTH).forEach(i -> {
-            Intersection intersection = camera.getScene().getIntersection(position, camera.getRight().multiply(xValues[i]).add(forward).normalize().multiply(Config.DRAW_DISTANCE).add(position));
-            if(intersection != null) {
-                int k = (int) (Config.HALF_HEIGHT / intersection.distance());
-                int start = Math.max(Config.HALF_HEIGHT - k, 0);
-                int end = Math.min(Config.HALF_HEIGHT + k, getHeight());
-                int frame = (end - start) * 3 / 100;
-                for(int j = start; j < start + frame; j++) {
-                    pixels[j * getWidth() + i] = 0;
-                }
-                for(int j = start + frame; j < end - frame; j++) {
-                    pixels[j * getWidth() + i] = intersection.obstacle().getColor(intersection.vector());
-                }
-                for(int j = end - frame; j < end; j++) {
-                    pixels[j * getWidth() + i] = 0;
-                }
-            }
-        });
-        g.drawImage(buffer, 0, 0, this);
+        Vector leftLimit = camera.getRight().multiply(-Config.ASPECT_RATIO).add(forward).normalize();
+        Vector rightLimit = camera.getRight().multiply(Config.ASPECT_RATIO).add(forward).normalize();
+        List<Layer> canvas = Scene.getCanvas(position, leftLimit, rightLimit, leftLimit.copy().add(position), rightLimit.copy().add(position));
+        for(Layer layer : canvas) {
+            int x1 = (int) (Config.SCREEN_WIDTH * layer.offset1());
+            int x2 = (int) (Config.SCREEN_WIDTH * layer.offset2());
+            int k1 = (int) (Config.HALF_HEIGHT / layer.distance1());
+            int k2 = (int) (Config.HALF_HEIGHT / layer.distance2());
+            int start1 = Math.max(Config.HALF_HEIGHT - k1, 0);
+            int end1 = Math.min(Config.HALF_HEIGHT + k1, getHeight());
+            int start2 = Math.max(Config.HALF_HEIGHT - k2, 0);
+            int end2 = Math.min(Config.HALF_HEIGHT + k2, getHeight());
+            g.setColor(layer.obstacle().getColor());
+            g.fillPolygon(new Polygon(new int[]{x1, x1, x2, x2}, new int[]{end1, start1, start2, end2}, 4));
+        }
         g.setColor(Color.WHITE);
         g.drawString((System.nanoTime() - startTime) / 1000000 + " ms", 10, 20);
     }
