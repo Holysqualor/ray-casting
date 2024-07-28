51879d94a3b18f67a37a05af1c016aa8083fffd5
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-07-28 10:00:31 +0200

    Update Wall.java

--- src/Wall.java
+++ src/Wall.java
@@ -13,40 +13,38 @@ public class Wall implements Obstacle {
     }
 
     @Override
-    public Layer project(Vector origin, Vector leftLimit, Vector rightLimit, Vector firstPixel, Vector lastPixel) {
-        Vector offset1 = Vector.lineIntersect(start, origin, firstPixel, lastPixel);
-        Vector offset2 = Vector.lineIntersect(end, origin, firstPixel, lastPixel);
-        Vector a = start, b = end;
-        if(offset1 == null || offset2 == null) {
-            Vector c = Vector.rayIntLine(origin, leftLimit, a, b);
-            Vector d = Vector.rayIntLine(origin, rightLimit, a, b);
-            if(offset1 == null) {
-                if(c != null) {
-                    offset1 = firstPixel;
-                    a = c;
-                } else if(d != null) {
-                    offset1 = lastPixel;
-                    a = d;
-                } else {
-                    return null;
-                }
-            }
-            if(offset2 == null) {
-                if(d != null) {
-                    offset2 = lastPixel;
-                    b = d;
-                } else if(c != null) {
-                    offset2 = firstPixel;
-                    b = c;
-                } else {
-                    return null;
-                }
-            }
-        }
-        double distance1 = origin.getDistance(a), distance2 = origin.getDistance(b);
-        double overview = firstPixel.getDistance(lastPixel);
-        return new Layer(firstPixel.getDistance(offset1) / overview, firstPixel.getDistance(offset2) / overview, distance1, distance2, (distance1 + distance2) / 2, this);
-    }
+    Layer project(Vector origin, Vector left, Vector right) {
+	Vector p1 = a.copy().substract(origin).normalize();
+	Vector p2 = b.copy().substract(origin).normalize();
+	Vector int1 = a, int2 = b;
+	if(!p1.isBetween(left, right)) {
+		if(p1.getDistance(left) < p1.getDistance(right)) {
+			p1 = left;
+			int1 = Vector.rayIntLine(origin, left, a, b);
+		} else {
+			p1 = right;
+			int1 = Vector.rayIntLine(origin, right, a, b);
+		}
+		if(int1 == null) {
+			return null;
+		}
+	}
+	if(!p2.isBetween(left, right)) {
+		if(p2.getDistance(left) < p2.getDistance(right)) {
+			p2 = left;
+			int2 = Vector.rayIntLine(origin, left, a, b);
+		} else {
+			p2 = right;
+			int2 = Vector.rayIntLine(origin, right, a, b);
+		}
+		if(int2 == null) {
+			return null;
+		}
+	}
+double dist1 = origin.getDistance(int1);
+double dist2 = origin.getDistance(int2);
+	return new Layer(left.getDistance(p1), left.getDistance(p2), dist1, dist2, (dist1 + dist2) / 2, this);
+}
 
     @Override
     public boolean belongs(Vector position) {
